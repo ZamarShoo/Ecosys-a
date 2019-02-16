f48744c8de2d80c298cd607a2aca92cc331510c8
zamarshoo <shg.ivan97@gmail.com>
2019-02-16 15:06:00 +0300

    script

--- js/common.js
+++ js/common.js
@@ -7,3 +7,18 @@ $('.mobileMenu').hide();
 $(document).ready(function(){
 
 });
+
+const anchors = document.querySelectorAll('a[href="about"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault()
+
+        const blockID = anchor.getAttribute('href')
+
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+}
